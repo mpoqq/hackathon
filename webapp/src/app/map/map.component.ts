f3ba31207a019b847073fdb3fd693e31327e0104
Denis Schwachhofer <56695884+Animu5@users.noreply.github.com>
2020-10-11 10:54:26 +0200

    add dist for github pages

--- webapp/src/app/map/map.component.ts
+++ webapp/src/app/map/map.component.ts
@@ -1,175 +1,160 @@
-import { ObserversModule } from '@angular/cdk/observers';
-import { ChangeDetectorRef, Component, ElementRef, OnInit, ReflectiveInjector, ViewChild } from '@angular/core';
+import { ObserversModule } from "@angular/cdk/observers";
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ReflectiveInjector,
+  ViewChild,
+} from "@angular/core";
 import * as L from "leaflet";
-import 'leaflet.heat/dist/leaflet-heat.js'
-import { RestService, Details, OSM, Tile } from './rest.service';
+import "leaflet.heat/dist/leaflet-heat.js";
+import { RestService, Details, OSM, Tile } from "./rest.service";
 
 interface IHeadLayer {
   heatLayer(points: any[], obj: object);
 }
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.scss"],
 })
 export class MapComponent implements OnInit {
-  private readonly inKm = 1/111 / 10;
+  private readonly inKm = 1 / 111 / 10;
   private readonly centerLong = 48.4060822;
   private readonly centerLat = 9.9876076;
   private heat: any;
   private curRect: any[];
 
-  @ViewChild('tooltip')
+  @ViewChild("tooltip")
   tooltipRef: ElementRef<HTMLElement>;
-  address = '';
-  
+  address = "";
 
   options = {
     layers: [
-        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 16, attribution: '...' })
+      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 16,
+        attribution: "...",
+      }),
     ],
     zoom: 14,
-    center: L.latLng(this.centerLong - this.inKm * 13, this.centerLat + this.inKm * 13)
+    center: L.latLng(
+      this.centerLong - this.inKm * 13,
+      this.centerLat + this.inKm * 13
+    ),
   };
 
-  layers = [ ];
+  layers = [];
 
   constructor(
     private chRef: ChangeDetectorRef,
-    private restService: RestService,
+    private restService: RestService
   ) {}
 
-  ngOnInit(): void { }
-  
-   newAddressPoints = [];
-  onMapReady(map) {
-    
-    
-    //const heat = L.heatLayer(newAddressPoints).addTo(map);
+  ngOnInit(): void {}
 
-    
+  newAddressPoints = [];
+  onMapReady(map) {
     this.restService.getAllTiles().subscribe();
 
-       
     this.restService.tiles.subscribe((tiles: Tile[]) => {
       this.newAddressPoints = [];
-      let max = 0;  
+      let max = 0;
       tiles.forEach((tile: Tile) => {
-    
         if (avg(tile) > max) {
           max = avg(tile);
         }
-        
-        this.newAddressPoints.push([tile.latitude, tile.longitude, avg(tile)])
-      })
 
-      this.newAddressPoints.forEach(x => x[2] = x[2] / max);
-      console.log(max)
-      
-      if(this.heat) {
+        this.newAddressPoints.push([tile.latitude, tile.longitude, avg(tile)]);
+      });
+
+      this.newAddressPoints.forEach((x) => (x[2] = x[2] / max));
+      console.log(max);
+
+      if (this.heat) {
         this.heat.remove();
       }
-      this.heat = (L as any as IHeadLayer).heatLayer(this.newAddressPoints, 
-        {
-          radius: 30,
-          blur: 20,
-          gradient: {
-            0.0: 'red',
-            0.9: 'yellow',
-            1.0: 'green'
-          }
-        });
+      this.heat = ((L as any) as IHeadLayer).heatLayer(this.newAddressPoints, {
+        radius: 30,
+        blur: 20,
+        gradient: {
+          0.0: "red",
+          0.9: "yellow",
+          1.0: "green",
+        },
+      });
       this.heat = this.heat.addTo(map);
-      //this.heat.remove();
-      
-      
     });
 
-
-    map.on('mousemove', this.mouseMv.bind(this));
+    map.on("mousemove", this.mouseMv.bind(this));
   }
 
-
-  mouseMv(e){
-
-    
+  mouseMv(e) {
     const lat = e.latlng.lat;
     const lng = e.latlng.lng;
 
-    
     const rect = [
-      [lat - lat % this.inKm, lng - lng % this.inKm],
-      [lat - lat % this.inKm + this.inKm, lng - lng % this.inKm],
-      [lat - lat % this.inKm + this.inKm, lng - lng % this.inKm + this.inKm],
-      [lat - lat % this.inKm, lng - lng % this.inKm + this.inKm],
+      [lat - (lat % this.inKm), lng - (lng % this.inKm)],
+      [lat - (lat % this.inKm) + this.inKm, lng - (lng % this.inKm)],
+      [
+        lat - (lat % this.inKm) + this.inKm,
+        lng - (lng % this.inKm) + this.inKm,
+      ],
+      [lat - (lat % this.inKm), lng - (lng % this.inKm) + this.inKm],
     ];
 
     if (JSON.stringify(this.curRect) == JSON.stringify(rect)) {
       return;
     }
 
-    
-
     this.restService.getDetails([1, 2]).subscribe((x: Details[]) => {
-      this.tooltipRef.nativeElement.innerHTML = '';
+      this.tooltipRef.nativeElement.innerHTML = "";
       const types = new Map<string, number>();
       x.forEach((t: Details) => {
         let amount = 0;
         if (types.has(t.type)) {
           amount = types.get(t.type);
         }
-        types.set(t.type, amount+1)
+        types.set(t.type, amount + 1);
       });
-      
-      this.tooltipRef.nativeElement.innerHTML = '<b>Ø 3km</b><br>'
+
+      this.tooltipRef.nativeElement.innerHTML = "<b>Ø 3km</b><br>";
       types.forEach((val, key) => {
-        this.tooltipRef.nativeElement.innerHTML += `${key}: ${val}<br>`
+        this.tooltipRef.nativeElement.innerHTML += `${key}: ${val}<br>`;
       });
-      
-    
-      //console.log(x);
     });
 
-    
     this.curRect = rect;
 
     this.layers = [];
     const polygon = L.polygon(this.curRect, {
-      color: 'white'
-    })
+      color: "white",
+    });
 
     const popup = polygon.bindPopup("Infos");
-    popup.addEventListener('click', () => {
+    popup.addEventListener("click", () => {
       this.restService.getSpecificInfos().subscribe((x: OSM.RootObject) => {
-        let res = '';
+        let res = "";
         for (const elem of x.elements) {
           res += elem.tags.name;
         }
         popup.setPopupContent(res);
       });
-      this.restService.reverseGeocode(this.curRect[0]).subscribe(x => {
+      this.restService.reverseGeocode(this.curRect[0]).subscribe((x) => {
         this.address = x;
-        //popup.setPopupContent(`<a href="https://www.immobilo.de/suchergebnisse?t=${x}&l=Ulm&a=de.ulm">Finde hier eine Wohnung!</a>`);
-      })
-    })
-    
+      });
+    });
 
     this.layers.push(polygon);
-    
+
     this.chRef.detectChanges();
-      
-		//this.heat.addLatLng(e.latlng);
   }
-  
-  
 }
 
-
-
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
+  var letters = "0123456789ABCDEF";
+  var color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
@@ -177,5 +162,13 @@ function getRandomColor() {
 }
 
 function avg(tile: Tile): number {
-  return ((tile.parkingLotScore + tile.restaurantScore + tile.transportationScore + tile.groceriesScore + tile.barScore) / 5) / 100;
+  return (
+    (tile.parkingLotScore +
+      tile.restaurantScore +
+      tile.transportationScore +
+      tile.groceriesScore +
+      tile.barScore) /
+    5 /
+    100
+  );
 }
--- webapp/src/app/map/rest.service.ts
+++ webapp/src/app/map/rest.service.ts
@@ -1,41 +1,39 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
-import { Geocode } from './geocode';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of, Subject } from "rxjs";
+import { Geocode } from "./geocode";
+import { map, tap } from "rxjs/operators";
 
 export declare module OSM {
-
   export interface Tags {
-      brand: string;
-      name: string;
-      shop: string;
-      toilets: string;
-      wheelchair: string;
+    brand: string;
+    name: string;
+    shop: string;
+    toilets: string;
+    wheelchair: string;
   }
 
   export interface Element {
-      type: string;
-      id: number;
-      lat: number;
-      lon: number;
-      timestamp: Date;
-      version: number;
-      changeset: number;
-      user: string;
-      uid: number;
-      tags: Tags;
+    type: string;
+    id: number;
+    lat: number;
+    lon: number;
+    timestamp: Date;
+    version: number;
+    changeset: number;
+    user: string;
+    uid: number;
+    tags: Tags;
   }
 
   export interface RootObject {
-      version: string;
-      generator: string;
-      copyright: string;
-      attribution: string;
-      license: string;
-      elements: Element[];
+    version: string;
+    generator: string;
+    copyright: string;
+    attribution: string;
+    license: string;
+    elements: Element[];
   }
-
 }
 export class Tile {
   id: string;
@@ -45,10 +43,9 @@ export class Tile {
   transportationScore: number;
   parkingLotScore: number;
   barScore: number;
-  restaurantScore: number
+  restaurantScore: number;
 }
- 
-  
+
 export class Details {
   id: string;
   tilesId: string;
@@ -56,76 +53,79 @@ export class Details {
   distance: string;
   nodeId: string;
 
-constructor(id: string, tilesId: string, type: string, distance: string, nodeId: string) {
-  this.id = id;
-  this.tilesId = tilesId;
-  this.type = type;
-  this.distance = distance;
-  this.nodeId = nodeId;
-}
+  constructor(
+    id: string,
+    tilesId: string,
+    type: string,
+    distance: string,
+    nodeId: string
+  ) {
+    this.id = id;
+    this.tilesId = tilesId;
+    this.type = type;
+    this.distance = distance;
+    this.nodeId = nodeId;
+  }
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RestService {
-
-  public tiles = new BehaviorSubject<Tile[]>([])
+  public tiles = new BehaviorSubject<Tile[]>([]);
   public initialTiles = [];
 
-  constructor(private client: HttpClient) { }
+  constructor(private client: HttpClient) {}
 
   getDetails(coords: [number, number]): Observable<Details[]> {
     return of([
-      new Details("ID", "TilesID", "Bar", "" + Math.random() * 10000, "1015047079"),
-      new Details("ID", "TilesID", "Bar", "" + Math.random() * 10000, "1015047079"),
-      new Details("ID", "TilesID", "Studio", "" + Math.random() * 10000, "1015047079")
+      new Details(
+        "ID",
+        "TilesID",
+        "Bar",
+        "" + Math.random() * 10000,
+        "1015047079"
+      ),
+      new Details(
+        "ID",
+        "TilesID",
+        "Bar",
+        "" + Math.random() * 10000,
+        "1015047079"
+      ),
+      new Details(
+        "ID",
+        "TilesID",
+        "Studio",
+        "" + Math.random() * 10000,
+        "1015047079"
+      ),
     ]);
   }
 
   getSpecificInfos(): Observable<OSM.RootObject> {
-    return this.client.get<OSM.RootObject>('https://api.openstreetmap.org/api/0.6/node/1015047079.json');
+    return this.client.get<OSM.RootObject>(
+      "https://api.openstreetmap.org/api/0.6/node/1015047079.json"
+    );
   }
 
   getAllTiles(): Observable<Tile[]> {
-  /*  const inKm = 1/112 / 10;
-  const centerLong = 48.4060822;
-  const centerLat = 9.9876076;
-    const tiles = [];
-    for (let i = 0; i < 30; i++) {
-      for (let j = -20; j < 0; j++) {
-        tiles.push({
-          id: "ID",
-          latitude: centerLong + inKm * j,
-          longitude: centerLat + inKm * i,
-          groceriesScore: Math.random(),
-          transportationScore: Math.random(),
-          parkingLotScore: Math.random(),
-          barScore: Math.random(),
-          restaurantScore: Math.random()
-        });
-      }
-    }  
-    this.initialTiles = JSON.parse(JSON.stringify(tiles));
-      
-    this.tiles.next(tiles);
-
-    return of(tiles);*/
-
-    
-     return this.client.get<Tile[]>('https://e97cdbfd571e.ngrok.io/api/tiles').pipe(tap((tiles: Tile[])=> {
-        this.initialTiles = tiles;
-        this.tiles.next(JSON.parse(JSON.stringify(tiles)));
+    return this.client
+      .get<Tile[]>("https://e97cdbfd571e.ngrok.io/api/tiles")
+      .pipe(
+        tap((tiles: Tile[]) => {
+          this.initialTiles = tiles;
+          this.tiles.next(JSON.parse(JSON.stringify(tiles)));
         })
       );
-    
-
-
-    return this.tiles;
   }
 
   reverseGeocode(coord: [number, number]): Observable<string> {
-      return this.client.get<Geocode>(`https://api.opencagedata.com/geocode/v1/json?key=255440e4c59440e09a2c30126391b76a&q=${coord[0]},${coord[1]}&pretty=1`).pipe(
+    return this.client
+      .get<Geocode>(
+        `https://api.opencagedata.com/geocode/v1/json?key=255440e4c59440e09a2c30126391b76a&q=${coord[0]},${coord[1]}&pretty=1`
+      )
+      .pipe(
         map((g: Geocode) => {
           if (g.results.length === 0) {
             return "";
@@ -148,5 +148,4 @@ export class RestService {
         })
       );
   }
-
 }
